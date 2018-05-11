37469f2ef005bf6ac2f600f343b02eb0bbb98c15
knuch <knuchelf@gmail.com>
2018-05-11 16:41:11 +0200

    use object buffering for shortcode output

--- shortcodes/news/controller.php
+++ shortcodes/news/controller.php
@@ -1,12 +1,16 @@
 <?php
 
 function renderNews ($news) {
+  ob_start();
+
   echo '<div class="news-listing">';
   foreach($news as $new) {
     set_query_var('epfl_shortcode_news_data', $new);
     get_template_part('shortcodes/news/view');
   }
   echo '</div>';
+
+  return ob_flush();
 }
 
-add_filter('epfl_shortcode_news', 'renderNews');
+add_action('epfl_shortcode_news', 'renderNews', 0, 1);
