c3508b159199c3fe0472ea1e0d13381d4acaeb12
knuch <knuchelf@gmail.com>
2018-05-30 16:26:32 +0200

    allow display of shortcode in backedn

--- shortcodes/news/controller.php
+++ shortcodes/news/controller.php
@@ -1,16 +1,12 @@
 <?php
 
 function renderNews ($news) {
-  ob_start();
-
   echo '<div class="news-listing">';
   foreach($news as $new) {
     set_query_var('epfl_shortcode_news_data', $new);
     get_template_part('shortcodes/news/view');
   }
   echo '</div>';
-
-  return ob_flush();
 }
 
 add_action('epfl_shortcode_news', 'renderNews', 0, 1);
