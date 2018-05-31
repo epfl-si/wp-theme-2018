2c75411a99f76828d2f549b0d7946731469f40a8
knuch <knuchelf@gmail.com>
2018-05-31 16:02:40 +0200

    correct script enqueuing

--- functions.php
+++ functions.php
@@ -124,10 +124,10 @@ function epfl_scripts() {
 
 	wp_enqueue_style( 'epfl-vendors', get_stylesheet_directory_uri().'/assets/css/vendors.min.css' );
 	wp_enqueue_style( 'epfl-base', get_stylesheet_directory_uri().'/assets/css/base.css' );
-
-	wp_enqueue_script( 'epfl-js-vendors', get_template_directory_uri() . '/assets/js/vendors.min.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'epfl-js-vendors-bundle', get_template_directory_uri() . '/assets/js/vendors.bundle.js', array(), '1.0.0', true );
-	wp_enqueue_script( 'epfl-js', get_template_directory_uri() . '/assets/js/app.bundle.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'epfl-js-jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', array(), false, true );
+	wp_enqueue_script( 'epfl-js-vendors', get_template_directory_uri() . '/assets/js/vendors.min.js', array('epfl-js-jquery'), '1.0.0', true );
+	wp_enqueue_script( 'epfl-js-vendors-bundle', get_template_directory_uri() . '/assets/js/vendors.bundle.js', array('epfl-js-vendors'), '1.0.0', true );
+	wp_enqueue_script( 'epfl-js', get_template_directory_uri() . '/assets/js/app.bundle.js', array('epfl-js-vendors-bundle'), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'epfl_scripts' );
 
