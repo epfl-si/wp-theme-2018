85fa9c401dfa3fe91d8d798aeb189a0bb1c79c2d
knuch <knuchelf@gmail.com>
2018-06-15 16:42:50 +0200

    make theme zippable

--- shortcodes/_load.php
+++ shortcodes/_load.php
@@ -1,14 +1,20 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+// require_once __DIR__.'/../vendor/autoload.php';
 
-use Symfony\Component\Finder\Finder;
+// use Symfony\Component\Finder\Finder;
 
 add_action( 'init', 'register_shortcodes' );
 
 // load all files named "controller.php" in all sub-folders
-$finder = new Finder();
-$finder->files()->in(__DIR__)->name('/controller.php$/');
-foreach ($finder as $file) {
+//$finder = new Finder();
+//$finder->files()->in(__DIR__)->name('/controller.php$/');
+/* foreach ($finder as $file) {
     require_once($file->getRealPath());
 }
+ */
+
+require_once(get_template_directory().'/shortcodes/collapsable/controller.php');
+require_once(get_template_directory().'/shortcodes/memento/controller.php');
+require_once(get_template_directory().'/shortcodes/news/controller.php');
+require_once(get_template_directory().'/shortcodes/page_teaser/controller.php');
