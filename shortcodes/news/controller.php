5412ad4aaccea14fec49551d281ff9e637737b84
knuch <knuchelf@gmail.com>
2018-07-17 11:24:20 +0200

    adapt news

--- shortcodes/news/controller.php
+++ shortcodes/news/controller.php
@@ -1,18 +1,18 @@
 <?php
 
-add_action('epfl_news_action', 'renderNews');
+add_action('epfl_news_action', 'renderNews', 10, 4);
 
-function renderNews ($news) {
-  ob_start();
+function renderNews ($title, $actus, $template, $stickers) {
   if (is_admin()) {
     // render placeholder for backend editor
     set_query_var('epfl_placeholder_title', 'News');
     get_template_part('shortcodes/placeholder');
   } else {
-    $results = $news->results;
+    $results = $actus->results;
     echo '<div class="container-full">';
     echo '<div class="container">';
     echo '<div class="news-listing my-5">';
+
     foreach($results as $new) {
       set_query_var('epfl_news_data', $new);
       get_template_part('shortcodes/news/view');
@@ -21,5 +21,4 @@ function renderNews ($news) {
     echo '</div>';
     echo '</div>';
   }
-  return ob_end_flush();
 }
