35caf6a7c6dc7e70de215b3ebc1abc098ba4f326
GregLeBarbar <gregory.charmier.epfl.ch>
2018-08-28 15:35:18 +0200

    epfl-card render

---
+++ shortcodes/epfl_card/view.php
@@ -0,0 +1,16 @@
+<?php
+    $title = get_query_var('epfl_card_title');
+    $text  = get_query_var('epfl_card_text');
+    $link  = get_query_var('epfl_card_link');
+    $image = get_query_var('epfl_card_image');
+?>
+
+<a href="<?php echo $link; ?>" class="card link-trapeze-horizontal">
+  <picture class="card-img-top">
+    <img src="<?php echo $image; ?>" class="img-fluid" title="<?php echo $title; ?>" alt="<?php echo $title; ?>" />
+  </picture>
+  <div class="card-body">
+    <h3 class="card-title"><?php echo $title; ?></h3>
+    <p><?php echo $text; ?></p>
+  </div>
+</a>
