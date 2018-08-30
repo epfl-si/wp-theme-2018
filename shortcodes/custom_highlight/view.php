dad3136ae31c18ad8609a3fd9a8435b6ad04c223
Marc Friederich <marc@antistatique.net>
2018-08-30 10:28:48 +0200

    Shortcode custom fullwidth teaser handle image the right way

--- shortcodes/custom_highlight/view.php
+++ shortcodes/custom_highlight/view.php
@@ -5,22 +5,30 @@ $data = get_query_var('epfl_custom_highlight_data');
 $classes = '';
 if ($data['layout'] == 'bottom') $classes = 'fullwidth-teaser-horizontal';
 if ($data['layout'] == 'left') $classes = 'fullwidth-teaser-left';
-
+if ($data['layout'] == '') $classes = 'fullwidth-teaser-right';
 ?>
 
 <div class="container-full">
   <div class="fullwidth-teaser mt-5  <?php echo $classes; ?>">
-    <picture>
-      <?php echo wp_get_attachment_image(
-        $data['image'],
-        'thumbnail_16_9_large', // see functions.php
-        '',
-        [
-          'class' => '',
-          'title' => $data['title']
-        ]
-      ) ?>
-    </picture>
+    <figure itemprop="associatedMedia" itemscope itemtype="http://schema.org/ImageObject" class="cover">
+      <!-- <picture> -->
+        <?php // get/slice image informations
+        $image_src = wp_get_attachment_image_src($data['image'], 'thumbnail_16_9_large')[0]; // see functions.php
+        $image_caption = wp_get_attachment_caption($data['image']);
+        $image_alt = get_post_meta($data['image'] , '_wp_attachment_image_alt', true);
+         ?>
+        <img src="<?php echo $image_src ?>" alt="<?php echo $image_alt ?>" />
+      <!-- </picture> -->
+      <?php if ($image_caption): ?>
+        <figcaption>
+          <button aria-hidden="true" type="button" class="btn-circle" data-toggle="popover" data-content="<?php echo $image_caption ?>">
+            <svg class="icon" aria-hidden="true"><use xlink:href="#icon-info"></use></svg>
+            <svg class="icon icon-rotate-90" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg>
+          </button>
+          <p class="sr-only"><?php echo $image_caption ?></p>
+        </figcaption>
+      <?php endif; ?>
+    </figure>
     <div class="fullwidth-teaser-text">
 
       <div class="fullwidth-teaser-header">
