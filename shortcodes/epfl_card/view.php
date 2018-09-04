3fee42d9f39e150b550832c7376c6c4df9d6e9de
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-04 10:21:25 +0200

    Test if user has post image

--- shortcodes/epfl_card/view.php
+++ shortcodes/epfl_card/view.php
@@ -6,9 +6,11 @@
 ?>
 
 <a href="<?php echo $link; ?>" class="card link-trapeze-horizontal">
+  <?php if ($image): ?> 
   <picture class="card-img-top">
     <img src="<?php echo $image; ?>" class="img-fluid" title="<?php echo $title; ?>" alt="<?php echo $title; ?>" />
   </picture>
+  <?php endif ?>
   <div class="card-body">
     <h3 class="card-title"><?php echo $title; ?></h3>
     <p><?php echo $text; ?></p>
