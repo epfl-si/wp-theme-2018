219c98135660cbf7d4d68013749bd463e66aeac8
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-05 07:32:02 +0200

    links box or links teaser

--- shortcodes/epfl_links_group/view.php
+++ shortcodes/epfl_links_group/view.php
@@ -2,10 +2,16 @@
     $links    = get_query_var('epfl_links_group_links');
     $main_url = get_query_var('epfl_links_group_main_url');
     $title    = get_query_var('epfl_links_group_title');
+    $is_links_teaser = ("" !== $main_url);
 ?>
-
-<div class="links-group">
-  <h5 id="links-group-title"><?php echo $title ?></h5>
+<div class="links-group <?php if ($is_links_teaser):?>links-group-teaser<?php endif; ?>">
+  <h5 id="links-group-title">
+    <?php if($is_links_teaser): ?>
+    <a class="link-pretty" href="<?php echo $main_url; ?>"><?php echo $title; ?></a>
+    <?php else: ?>
+    <?php echo $title; ?>
+    <?php endif; ?>
+  </h5>
   <nav
     class="nav flex-column flex-wrap align-items-start"
     role="navigation"
