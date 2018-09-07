2d95ca5af56d5b9ca2ee60a03c08491416305458
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-07 13:22:36 +0200

    delete old css classes

--- shortcodes/epfl_video/view.php
+++ shortcodes/epfl_video/view.php
@@ -5,7 +5,7 @@
 ?>
 
 <div class="container">
-  <div class="epfl-video epfl-video-responsive embed-responsive embed-responsive-16by9">
+  <div class="embed-responsive embed-responsive-16by9">
     <iframe src="<?php echo $url ?>" width="<?php echo $width ?>" height="<?php echo $height ?>" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" frameborder="0" class="embed-responsive-item"></iframe>
   </div>
 </div>
