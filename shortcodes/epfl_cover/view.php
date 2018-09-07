280518832b628600610ea7c4827a3a1ffb88b9e6
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-07 08:23:02 +0200

    Add container class

--- shortcodes/epfl_cover/view.php
+++ shortcodes/epfl_cover/view.php
@@ -3,21 +3,23 @@
     $image = get_query_var('epfl_cover_image');
 ?>
 
-<figure class="cover">
-  <picture>
-    <img src="<?php echo $image; ?>" class="img-fluid" alt="<?php echo $description; ?>">
-  </picture>
-  <figcaption>
-    <button
-      aria-hidden="true"
-      type="button"
-      class="btn-circle"
-      data-toggle="popover"
-      data-content="<?php echo $description; ?>"
-    >
-      <svg class="icon" aria-hidden="true"><use xlink:href="#icon-info"></use></svg>
-      <svg class="icon icon-rotate-90" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg>
-    </button>
-    <p class="sr-only"><?php echo $description; ?></p>
-    </figcaption>
-</figure>
+<div class="container">
+  <figure class="cover">
+    <picture>
+      <img src="<?php echo $image; ?>" class="img-fluid" alt="<?php echo $description; ?>">
+    </picture>
+    <figcaption>
+      <button
+        aria-hidden="true"
+        type="button"
+        class="btn-circle"
+        data-toggle="popover"
+        data-content="<?php echo $description; ?>"
+      >
+        <svg class="icon" aria-hidden="true"><use xlink:href="#icon-info"></use></svg>
+        <svg class="icon icon-rotate-90" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg>
+      </button>
+      <p class="sr-only"><?php echo $description; ?></p>
+      </figcaption>
+  </figure>
+</div>
