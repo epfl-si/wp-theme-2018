9a994cc50dc6f35b4f036aaaa5ba19ac86402d85
Flavien Knuchel <knuchelf@gmail.com>
2018-09-11 12:53:40 +0200

    refactor faculty to schools for all visible parts (#76)
    * refactor faculty to schools for all visible parts
    
    * second part of refactoring, only leaving the shortcode name with the word faculty

--- shortcodes/schools/controller.php
+++ shortcodes/schools/controller.php
@@ -3,20 +3,20 @@
 require_once(__DIR__.'/shortcake.php');
 
 // register
-add_shortcode( 'epfl_faculties', 'renderFaculties' );
+add_shortcode( 'epfl_faculties', 'renderSchools' );
 
 // render
-function renderFaculties ($args) {
-  set_query_var('epfl_faculties_data', $args);
+function renderSchools ($args) {
+  set_query_var('epfl_schools_data', $args);
 
   ob_start();
   if (is_admin()) {
     // render placeholder for backend editor
-    set_query_var('epfl_placeholder_title', 'Faculties');
+    set_query_var('epfl_placeholder_title', 'Schools');
     get_template_part('shortcodes/placeholder');
   } else {
     // render shortcode
-    get_template_part('shortcodes/faculties/view');
+    get_template_part('shortcodes/schools/view');
   }
   return ob_get_clean();
 }
