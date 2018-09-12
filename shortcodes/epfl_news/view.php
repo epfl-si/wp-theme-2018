7b9e803b17ccb21f03c74417fa1d3043c52a52c9
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-12 08:34:00 +0200

    escape html epfl-news

--- shortcodes/epfl_news/view.php
+++ shortcodes/epfl_news/view.php
@@ -57,18 +57,18 @@
 <?php
   if ("1" == $template): // TEMPLATE LISTING
 ?>
-        <a href="<?php echo $news->news_url ?>" class="list-group-item list-group-teaser link-trapeze-vertical">
+        <a href="<?php echo esc_url($news->news_url) ?>" class="list-group-item list-group-teaser link-trapeze-vertical">
           <div class="list-group-teaser-container">
             <div class="list-group-teaser-thumbnail">
               <picture>
-                <img src="<?php echo $visual_url ?>" class="img-fluid" alt="<?php echo $image_description ?>" >
+                <img src="<?php echo esc_url($visual_url) ?>" class="img-fluid" alt="<?php echo esc_attr($image_description) ?>" >
               </picture>
             </div>
             <div class="list-group-teaser-content">
-              <p class="h5"><?php echo $news->title ?></p>
+              <p class="h5"><?php echo esc_html($news->title) ?></p>
               <p>
-                <time datetime="<?php echo $publish_date ?>"><span class="sr-only">Published:</span><?php echo $publish_date ?></time>
-                <span class="text-muted">— <?php echo $subtitle ?></span>
+                <time datetime="<?php echo esc_attr($publish_date) ?>"><span class="sr-only">Published:</span><?php echo esc_html($publish_date) ?></time>
+                <span class="text-muted">— <?php echo esc_html($subtitle) ?></span>
               </p>
             </div>
           </div>
@@ -79,28 +79,28 @@
 
         <div class="fullwidth-teaser fullwidth-teaser-horizontal">
           <picture>
-            <img src="<?php echo $visual_url ?>" aria-labelledby="background-label" alt="<?php echo $image_description ?>"/>
+            <img src="<?php echo esc_url($visual_url) ?>" aria-labelledby="background-label" alt="<?php echo esc_attr($image_description) ?>"/>
           </picture>
           <div class="fullwidth-teaser-text">
             <div class="fullwidth-teaser-header">
               <div class="fullwidth-teaser-title">
                 <h3><?php echo $news->title ?></h3>
                 <ul class="list-inline mt-2">
-                  <li class="list-inline-item"><?php esc_html_e('News', 'epfl');?></li>
-                  <li class="list-inline-item"><?php echo $category ?></li>
+                  <li class="list-inline-item"><?php esc_html_e('News', 'epfl') ?></li>
+                  <li class="list-inline-item"><?php echo esc_html($category) ?></li>
                 </ul>
               </div>
-              <a href="<?php echo $news->news_url ?>" aria-label="Link to read more of that page" class="btn btn-primary triangle-outer-top-right d-none d-xl-block">
+              <a href="<?php echo esc_url($news->news_url) ?>" aria-label="Link to read more of that page" class="btn btn-primary triangle-outer-top-right d-none d-xl-block">
                 <?php esc_html_e('Read more', 'epfl');?>
                 <span class="sr-only">sur Tech Transfer.</span>
                 <svg class="icon" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg>
               </a>
             </div>
             <div class="fullwidth-teaser-content">
-              <p><?php echo $subtitle ?></p>
+              <p><?php echo esc_html($subtitle) ?></p>
             </div>
             <div class="fullwidth-teaser-footer">
-              <a href="<?php echo $news->news_url ?>" aria-label="Link to read more of that page" class="btn btn-primary btn-block d-xl-none"><?php esc_html_e('Read more', 'epfl');?></a>
+              <a href="<?php echo esc_url($news->news_url) ?>" aria-label="Link to read more of that page" class="btn btn-primary btn-block d-xl-none"><?php esc_html_e('Read more', 'epfl');?></a>
             </div>
           </div>
         </div>
@@ -111,28 +111,28 @@
 
         <div class="fullwidth-teaser fullwidth-teaser-horizontal">
           <picture>
-            <img src="<?php echo $visual_url ?>" aria-labelledby="background-label" alt="<?php echo $image_description ?>"/>
+            <img src="<?php echo esc_url($visual_url) ?>" aria-labelledby="background-label" alt="<?php echo esc_attr($image_description) ?>"/>
           </picture>
           <div class="fullwidth-teaser-text">
             <div class="fullwidth-teaser-header">
               <div class="fullwidth-teaser-title">
-                <h3><?php echo $news->title ?></h3>
+                <h3><?php echo esc_html($news->title) ?></h3>
                 <ul class="list-inline mt-2">
-                  <li class="list-inline-item"><?php esc_html_e('News', 'epfl');?></li>
-                  <li class="list-inline-item"><?php echo $category ?></li>
+                  <li class="list-inline-item"><?php esc_html_e('News', 'epfl') ?></li>
+                  <li class="list-inline-item"><?php echo esc_html($category) ?></li>
                 </ul>
               </div>
-              <a href="<?php echo $news->news_url ?>" aria-label="Link to read more of that page" class="btn btn-primary triangle-outer-top-right d-none d-xl-block">
+              <a href="<?php echo esc_url($news->news_url) ?>" aria-label="Link to read more of that page" class="btn btn-primary triangle-outer-top-right d-none d-xl-block">
                 <?php esc_html_e('Read more', 'epfl');?>
                 <span class="sr-only">sur Tech Transfer.</span>
                 <svg class="icon" aria-hidden="true"><use xlink:href="#icon-chevron-right"></use></svg>
               </a>
             </div>
             <div class="fullwidth-teaser-content">
-              <p><?php echo $subtitle ?></p>
+              <p><?php echo esc_html($subtitle) ?></p>
             </div>
             <div class="fullwidth-teaser-footer">
-              <a href="<?php echo $news->news_url ?>" aria-label="Link to read more of that page" class="btn btn-primary btn-block d-xl-none"><?php esc_html_e('Read more', 'epfl');?></a>
+              <a href="<?php echo esc_url($news->news_url) ?>" aria-label="Link to read more of that page" class="btn btn-primary btn-block d-xl-none"><?php esc_html_e('Read more', 'epfl');?></a>
             </div>
           </div>
         </div>
@@ -140,17 +140,17 @@
   <?php else: ?>
 
         <div class="col-md-6 d-flex">
-          <a href="<?php echo $news->news_url ?>" class="card link-trapeze-horizontal">
+          <a href="<?php echo esc_url($news->news_url) ?>" class="card link-trapeze-horizontal">
             <div class="card-body">
-              <h3 class="card-title"><?php echo $news->title ?></h3>
+              <h3 class="card-title"><?php echo esc_html($news->title) ?></h3>
               <div class="card-info">
                 <span class="card-info-date">
-                  <time datetime="DATETIME HERE"><?php echo $publish_date ?></time>
+                  <time datetime="DATETIME HERE"><?php echo esc_html($publish_date) ?></time>
                 </span>
                 <span><?php esc_html_e('News', 'epfl');?></span>
-                <span><?php echo $category ?></span>
+                <span><?php echo esc_html($category) ?></span>
               </div>
-              <p><?php echo $subtitle ?></p>
+              <p><?php echo esc_html($subtitle) ?></p>
             </div>
           </a>
         </div>
