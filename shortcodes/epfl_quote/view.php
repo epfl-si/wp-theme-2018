f16a0378d7dcdb91222218de02804bbec79d9333
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-12 15:03:27 +0200

    escape html epfl-quote

--- shortcodes/epfl_quote/view.php
+++ shortcodes/epfl_quote/view.php
@@ -7,11 +7,11 @@ $image  = get_query_var('epfl_quote_image');
 <div class="row">
   <div class="col-6 offset-3 col-sm-4 offset-sm-4 col-md-2 offset-md-0 text-center text-md-right">
     <picture>
-      <img src="<?php echo esc_attr($image); ?>" class="img-fluid rounded-circle" alt="ALT">
+      <img src="<?php echo esc_url($image); ?>" class="img-fluid rounded-circle" alt="ALT">
     </picture>
   </div>
   <blockquote class="blockquote mt-3 col-md-10 border-0">
     <p class="mb-0"><?php echo esc_attr($quote); ?></p>
-    <footer class="blockquote-footer"><cite title="<?php echo esc_attr($cite); ?>"><?php echo esc_attr($cite); ?></cite>, <?php echo esc_attr($footer); ?></footer>
+    <footer class="blockquote-footer"><cite title="<?php echo esc_attr($cite); ?>"><?php echo esc_html($cite); ?></cite>, <?php echo esc_html($footer); ?></footer>
   </blockquote>
 </div>
