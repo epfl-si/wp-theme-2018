1b178502456d56709ef2f8a236390da274cd7d2b
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-12 16:48:23 +0200

    delete ;

--- shortcodes/epfl_toggle/view.php
+++ shortcodes/epfl_toggle/view.php
@@ -10,16 +10,16 @@
       class="collapse-title collapse-title-desktop collapsed"
       type="button"
       data-toggle="collapse"
-      data-target="<?php echo esc_attr('#collapse-' . $key); ?>"
+      data-target="<?php echo esc_attr('#collapse-' . $key) ?>"
       aria-expanded="false"
-      aria-controls="<?php echo esc_attr('#collapse-' . $key); ?>"
+      aria-controls="<?php echo esc_attr('#collapse-' . $key) ?>"
     >
-    <?php echo esc_html($value['label']); ?>
+    <?php echo esc_html($value['label']) ?>
   </button>
   <div 
-    class="collapse collapse-item collapse-item-desktop <?php if ($value['state'] === 'open'): ?> show <?php endif; ?> " 
+    class="collapse collapse-item collapse-item-desktop <?php if ($value['state'] === 'open'): ?> show <?php endif ?> " 
     id="<?php echo esc_attr('collapse' . '-' . $key) ?>"
   >
-    <p><?php echo wp_kses_post(urldecode($value['desc'])); ?></p>
+    <p><?php echo wp_kses_post(urldecode($value['desc'])) ?></p>
   </div>
 <?php endforeach; ?>
