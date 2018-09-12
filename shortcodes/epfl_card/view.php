e5d545b6d2ab8323dde6162cfcda5fb5236a37aa
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-12 13:43:08 +0200

    escape html epfl-card

--- shortcodes/epfl_card/view.php
+++ shortcodes/epfl_card/view.php
@@ -5,14 +5,14 @@
     $image = get_query_var('epfl_card_image');
 ?>
 
-<a href="<?php echo $link; ?>" class="card link-trapeze-horizontal">
+<a href="<?php echo esc_url($link) ?>" class="card link-trapeze-horizontal">
   <?php if ($image): ?> 
   <picture class="card-img-top">
-    <img src="<?php echo $image; ?>" class="img-fluid" title="<?php echo $title; ?>" alt="<?php echo $title; ?>" />
+    <img src="<?php echo esc_url($image) ?>" class="img-fluid" title="<?php echo esc_attr($title) ?>" alt="<?php echo esc_attr($title) ?>" />
   </picture>
   <?php endif ?>
   <div class="card-body">
-    <h3 class="card-title"><?php echo $title; ?></h3>
-    <p><?php echo $text; ?></p>
+    <h3 class="card-title"><?php echo esc_html($title) ?></h3>
+    <p><?php echo esc_html($text) ?></p>
   </div>
 </a>
