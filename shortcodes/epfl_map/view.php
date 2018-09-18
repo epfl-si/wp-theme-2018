5e485c6459fd1236c2d0d9282ab63e33cfe9840d
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-18 19:17:35 +0200

    fix lang parameter

--- shortcodes/epfl_map/view.php
+++ shortcodes/epfl_map/view.php
@@ -1,6 +1,6 @@
 <?php
-  $query  = get_query_var('epfl_map_query');
-  $height = get_query_var('epfl_map_height');
+  $query = get_query_var('epfl_map_query');
+  $lang  = get_query_var('epfl_map_lang');
 
   $video_url = 'https://plan.epfl.ch/iframe/?q=' . $query . '&amp;lang=' . $lang . '&amp;map_zoom=10';
 
