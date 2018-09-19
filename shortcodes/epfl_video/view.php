0987cbe98513768a1017c7c7109b64484d25abcb
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-19 10:09:14 +0200

    clean width and heigt parameters

--- shortcodes/epfl_video/view.php
+++ shortcodes/epfl_video/view.php
@@ -1,11 +1,9 @@
 <?php
-  $url    = get_query_var('epfl_video_url');
-  $width  = get_query_var('epfl_video_width');
-  $height = get_query_var('epfl_video_height');
+  $url = get_query_var('epfl_video_url');
 ?>
 
 <div class="container">
   <div class="embed-responsive embed-responsive-16by9">
-    <iframe src="<?php echo esc_url($url) ?>" width="<?php echo esc_attr($width) ?>" height="<?php echo esc_attr($height) ?>" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" frameborder="0" class="embed-responsive-item"></iframe>
+    <iframe src="<?php echo esc_url($url) ?>" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" frameborder="0" class="embed-responsive-item"></iframe>
   </div>
 </div>
