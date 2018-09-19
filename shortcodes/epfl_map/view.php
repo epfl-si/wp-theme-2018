32e361a63abacc186cd409c4777bedcbb20cad24
GregLeBarbar <gregory.charmier.epfl.ch>
2018-09-19 13:01:56 +0200

    rename a variable

--- shortcodes/epfl_map/view.php
+++ shortcodes/epfl_map/view.php
@@ -2,7 +2,7 @@
   $query = get_query_var('epfl_map_query');
   $lang  = get_query_var('epfl_map_lang');
 
-  $video_url = 'https://plan.epfl.ch/iframe/?q=' . $query . '&amp;lang=' . $lang . '&amp;map_zoom=10';
+  $map_url = 'https://plan.epfl.ch/iframe/?q=' . $query . '&amp;lang=' . $lang . '&amp;map_zoom=10';
 
 ?>
 <div class="container">
@@ -10,7 +10,7 @@
     <iframe 
       frameborder="0" 
       scrolling="no" 
-      src="<?php echo esc_url($video_url) ?>" 
+      src="<?php echo esc_url($map_url) ?>" 
       class="embed-responsive-item"
       ></iframe>
   </div>
