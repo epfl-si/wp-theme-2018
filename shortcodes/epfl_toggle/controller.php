24979354f9f79453167e6796ad244f9bfc10109e
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2018-09-25 06:48:42 +0200

    toggle v2 (#99)
    * toggle v2
    
    * improve icon open/close

--- shortcodes/epfl_toggle/controller.php
+++ shortcodes/epfl_toggle/controller.php
@@ -5,9 +5,9 @@
  * 4rth argument is number of arguments the function can accept
  **/
 
-add_action('epfl_toggle_action', 'renderToggle', 10, 1);
+add_action('epfl_toggle_action', 'renderToggle', 10, 3);
 
-function renderToggle ($args) {
+function renderToggle ($title, $state, $content) {
 
   if (is_admin()) {
 
@@ -17,13 +17,9 @@ function renderToggle ($args) {
 
   } else {
 
-    $fields = array();
-    foreach ($args as $key => $value) {
-      $toggle_key = substr($key, -1);
-      $field_name = substr($key, 0, -1);  
-      $fields[$toggle_key][$field_name] = $value;
-    }
-    set_query_var('epfl_toggle_data', $fields);
+    set_query_var('epfl_toggle_title', $title);
+    set_query_var('epfl_toggle_state', $state);
+    set_query_var('epfl_toggle_content', $content);
     get_template_part('shortcodes/epfl_toggle/view');
   }
 }
