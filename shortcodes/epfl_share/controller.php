154b45e72b8553a679f28d1e09bc9e3bf9d7e083
Julien Delasoie <julien.delasoie@epfl.ch>
2018-10-03 12:19:28 +0200

    Add open provided link into service providers

--- shortcodes/epfl_share/controller.php
+++ shortcodes/epfl_share/controller.php
@@ -5,15 +5,18 @@
  * 4rth argument is number of arguments the function can accept
  **/
 
-add_action('epfl_share_2018', 'renderShare', 10, 2);
+add_action('epfl_share_action', 'renderShare', 10, 3);
 
-function renderShare($image, $description) {
+function renderShare($post_title, $target_url, $target_url_encoded) {
   if (is_admin()) {
     // render placeholder for backend editor
     set_query_var('epfl_placeholder_title', 'Share buttons');
     get_template_part('shortcodes/placeholder');
   } else {
     // render for frontend
+    set_query_var('epfl_share_post_title', $post_title);
+    set_query_var('epfl_share_target_url', $target_url);
+    set_query_var('epfl_share_target_url_encoded', $target_url_encoded);
     get_template_part('shortcodes/epfl_share/view');
   }
 }
--- shortcodes/epfl_share/view.php
+++ shortcodes/epfl_share/view.php
@@ -1,4 +1,7 @@
-<?php /* here we commented the ones we don't use at the moment */
+<?php
+  $post_title         = get_query_var('epfl_share_post_title');
+  $target_url     = get_query_var('epfl_share_target_url');
+  $target_url_encoded = get_query_var('epfl_share_target_url_encoded');
 ?>
 
 <div class="bg-gray-100 py-5">
@@ -8,37 +11,37 @@
         <div class="social-share-text">
           Partager sur
         </div>
-        <a href="#" class="social-icon social-icon-facebook social-icon-discrete">
+        <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $target_url_encoded ?>" class="social-icon social-icon-facebook social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-facebook"></use></svg>
           <span class="sr-only">Follow us on Facebook.</span>
         </a>
-        <a href="#" class="social-icon social-icon-twitter social-icon-discrete">
+        <a href="https://twitter.com/intent/tweet?url=<?php echo $target_url_encoded ?>&amp;text=<?php echo $post_title ?>&amp;hashtags=epfl" class="social-icon social-icon-twitter social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-twitter"></use></svg>
           <span class="sr-only">Follow us on Twitter.</span>
         </a>
         <?php /*
-        <a href="#" class="social-icon social-icon-instagram social-icon-discrete">
+        <a href="#" class="social-icon social-icon-instagram social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-instagram"></use></svg>
           <span class="sr-only">Follow us on Instagram.</span>
         </a>
         */ ?>
         <?php /*
-        <a href="#" class="social-icon social-icon-mail-plane social-icon-discrete">
+        <a href="#" class="social-icon social-icon-mail-plane social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-mail-plane"></use></svg>
           <span class="sr-only">Follow us on Mailplane.</span>
         </a>
         */ ?>
         <?php /*
-        <a href="#" class="social-icon social-icon-whatsapp social-icon-discrete">
+        <a href="#" class="social-icon social-icon-whatsapp social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-whatsapp"></use></svg>
           <span class="sr-only">Follow us on WhatsApp.</span>
         </a>
         */ ?>
-        <a href="#" class="social-icon social-icon-googleplus social-icon-discrete">
+        <a href="https://plus.google.com/share?url=<?php echo $target_url_encoded ?>" class="social-icon social-icon-googleplus social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-googleplus"></use></svg>
           <span class="sr-only">Follow us on GooglePlus.</span>
         </a>
-        <a href="#" class="social-icon social-icon-linkedin social-icon-discrete">
+        <a href="https://www.linkedin.com/shareArticle?url=<?php echo $target_url_encoded ?>&amp;title=<?php echo $post_title ?>%20%23epfl" class="social-icon social-icon-linkedin social-icon-discrete" target="_blank">
           <svg class="icon" aria-hidden="true"><use xlink:href="#icon-linkedin"></use></svg>
           <span class="sr-only">Follow us on LinkedIn.</span>
         </a>
