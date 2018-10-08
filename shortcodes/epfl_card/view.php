0c54c9ad18169a90d951c19c4de50279291c3d54
jdelasoie <julien.delasoie@epfl.ch>
2018-10-08 11:24:14 +0200

    Allow to print multiple cards (#105)
    * Allow to print multiple cards
    * Allow to use only one card

--- shortcodes/epfl_card/view.php
+++ shortcodes/epfl_card/view.php
@@ -1,18 +1,49 @@
 <?php
-    $title = get_query_var('epfl_card_title');
-    $text  = get_query_var('epfl_card_text');
-    $link  = get_query_var('epfl_card_link');
-    $image = get_query_var('epfl_card_image');
-?>
+  $data = get_query_var('epfl_cards');
 
-<a href="<?php echo esc_url($link) ?>" class="card link-trapeze-horizontal">
-  <?php if ($image): ?> 
-  <picture class="card-img-top">
-    <img src="<?php echo esc_url($image) ?>" class="img-fluid" title="<?php echo esc_attr($title) ?>" alt="<?php echo esc_attr($title) ?>" />
-  </picture>
-  <?php endif ?>
-  <div class="card-body">
-    <h3 class="card-title"><?php echo esc_html($title) ?></h3>
-    <p><?php echo esc_html($text) ?></p>
+  if (!$data) return true;
+
+  $elementCount = 0;
+  for($i = 1; $i < 4; $i++){
+    if (strlen(sanitize_text_field($data['title'.$i])) > 0) {
+      $elementCount++;
+    }
+  }
+?>
+<?php if ($elementCount > 1): ?>
+<div class="container my-3">
+  <div class="card-deck <?php echo ($elementCount < 3) && ($elementCount > 1) ? ' card-deck-line' : '' ?>">
+<?php endif ?>
+    <?php
+    for($i = 1; $i < 4; $i++):
+      if ($data['title'.$i]) :
+      $image = get_post($data['image'.$i]);
+    ?>
+    <a href="<?php echo $data['url'.$i] ?: '#' ?>" class="card link-trapeze-horizontal">
+    <?php if ($data['image'.$i]): ?>
+      <picture class="card-img-top">
+      <?php echo wp_get_attachment_image(
+        $data['image'.$i],
+        'thumbnail_16_9_crop', // see functions.php
+        '',
+        [
+          'class' => 'img-fluid',
+          'title' => $image->post_excerpt
+        ]
+        ) ?>
+      </picture>
+    <?php endif; ?>
+      <div class="card-body">
+        <h3 class="card-title"><?php echo sanitize_text_field($data['title'.$i]) ?: 'Title' ?></h3>
+        <p><?php echo wp_kses_post(urldecode($data['content'.$i])) ?: '' ?></p>
+      </div>
+    </a>
+    <?php
+      endif;
+      endfor;
+    ?>
+<?php if ($elementCount > 1): ?>
   </div>
-</a>
+</div>
+<?php endif ?>
+
