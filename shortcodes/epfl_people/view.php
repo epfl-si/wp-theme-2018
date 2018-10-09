eafe2ef8827afd55ca203d1e363baf816926edbb
Julien Delasoie <julien.delasoie@epfl.ch>
2018-10-09 15:44:47 +0200

    Allow to set multiple columns on people render

--- shortcodes/epfl_people/view.php
+++ shortcodes/epfl_people/view.php
@@ -1,10 +1,13 @@
 <?php
   require_once('utils.php');
   $persons = get_query_var('epfl_people_persons');
+  $nb_column = get_query_var('epfl_people_nb_columns');
 ?>
 
+<div class="container">
+<div class="row">
 <?php 
-  foreach($persons as $person):
+  foreach($persons as $index => $person):
     $photo_url  = epfl_people_get_photo($person);
     $phones     = epfl_people_get_phones($person);
     $functions  = epfl_people_get_functions($person);
@@ -12,29 +15,36 @@
     $room_url   = epfl_people_get_room_url($rooms[0]);
     $people_url = epfl_people_get_people_url($person);
 ?>
-<div class="card">
-  <div class="card-body">
-    <div class="my-3 d-md-flex align-items-center">
-      <?php if ($photo_url): ?>
-        <img style="width:100px;" class="img-fluid rounded-circle mr-4" src="<?php echo esc_url($photo_url) ?>" alt="<?php echo esc_attr($person->prenom) ?> <?php echo esc_attr($person->nom) ?>">
-      <?php endif; ?>
-      <div class="w-100 mt-2 mt-md-0">
-        <?php if ($person->email): ?>
-        <a class="btn btn-block btn-primary mb-2" href="mailto:<?php echo esc_attr($person->email) ?>"><?php echo esc_html($person->email) ?></a>
-        <?php endif ?> 
-        <?php if ($phones[0]): ?>
-        <a class="btn btn-block btn-secondary" href="tel:<?php echo esc_html($phones[0]) ?>"><?php echo esc_html($phones[0]) ?></a>
-        <?php endif ?>
+<?php if ($index != 0 && $index % $nb_column == 0): ?></div><div class="row"><?php endif; ?>
+  <div class="card">
+    <div class="card-body">
+      <div class="my-3 d-md-flex align-items-center">
+        <?php if ($photo_url): ?>
+          <img style="width:100px;" class="img-fluid rounded-circle mr-4" src="<?php echo esc_url($photo_url) ?>" alt="<?php echo esc_attr($person->prenom) ?> <?php echo esc_attr($person->nom) ?>">
+        <?php endif; ?>
+        <div class="w-100 mt-2 mt-md-0">
+          <?php if ($person->email): ?>
+          <a class="btn btn-block btn-primary mb-2" href="mailto:<?php echo esc_attr($person->email) ?>"><?php echo esc_html($person->email) ?></a>
+          <?php endif ?>
+          <?php if ($phones[0]): ?>
+          <a class="btn btn-block btn-secondary" href="tel:<?php echo esc_html($phones[0]) ?>"><?php echo esc_html($phones[0]) ?></a>
+          <?php endif ?>
+        </div>
       </div>
+      <h3><a class="link-pretty" href="<?php echo esc_url($people_url) ?>"><?php echo esc_html($person->prenom) ?> <?php echo esc_html($person->nom) ?></a></h3>
+      <dl class="definition-list definition-list-grid mb-0">
+        <dt>Fonction</dt>
+        <dd><?php echo esc_html($functions[0]) ?></dd>
+        <dt>Bureau</dt>
+        <dd><a class="link-pretty" href="<?php echo esc_url($room_url) ?>"><?php echo esc_html($rooms[0]) ?></a></dd>
+      </dl>
     </div>
-    <h3><a class="link-pretty" href="<?php echo esc_url($people_url) ?>"><?php echo esc_html($person->prenom) ?> <?php echo esc_html($person->nom) ?></a></h3>
-    <dl class="definition-list definition-list-grid mb-0">
-      <dt>Fonction</dt>
-      <dd><?php echo esc_html($functions[0]) ?></dd>
-      <dt>Bureau</dt>
-      <dd><a class="link-pretty" href="<?php echo esc_url($room_url) ?>"><?php echo esc_html($rooms[0]) ?></a></dd>
-    </dl>
   </div>
+    <?php if($index % $nb_column == 0): ?></div"><?php endif; ?>
+  <?php endforeach; ?>
+  <?php for($i=$index+1; $i % $nb_column != 0; $i++): ?>
+  <div class="card">
+  </div>
+  <?php endfor; ?>
 </div>
-
-<?php endforeach ?>
+</div>
