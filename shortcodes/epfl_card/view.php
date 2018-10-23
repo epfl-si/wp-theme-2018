aa22a409c59e4f49924d12d2a156b7aaf79c0b7c
Julien Delasoie <julien.delasoie@epfl.ch>
2018-10-23 13:30:05 +0200

    Fix link parameter name; Allow to show a card without link

--- shortcodes/epfl_card/view.php
+++ shortcodes/epfl_card/view.php
@@ -18,10 +18,13 @@
     for($i = 1; $i < 4; $i++):
       if ($data['title'.$i]) :
       $image = get_post($data['image'.$i]);
+      $url = esc_url($data['link'.$i]);
     ?>
     <div class="card">
       <?php if ($data['image'.$i]): ?>
-      <a href="<?php echo esc_url($data['url'.$i]) ?: '#' ?>" class="card-img-top">
+        <?php if ($url): ?>
+      <a href="<?php echo $url ?: '#' ?>" class="card-img-top">
+        <?php endif; ?>
         <picture class="card-img-top">
         <?php echo wp_get_attachment_image(
           $data['image'.$i],
@@ -33,10 +36,16 @@
           ]
           ) ?>
         </picture>
+        <?php if ($url): ?>
       </a>
+        <?php endif; ?>
       <?php endif; ?>
         <div class="card-body">
-          <div class="card-title"><a href="<?php echo esc_url($data['url'.$i]) ?: '#' ?>" class="h3"><?php echo esc_html($data['title'.$i]) ?: '' ?></a></div>
+          <?php if ($url): ?>
+          <div class="card-title"><a href="<?php echo esc_url($data['link'.$i]) ?: '#' ?>" class="h3"><?php echo esc_html($data['title'.$i]) ?: '' ?></a></div>
+          <?php else: ?>
+          <div class="card-title"><div class="h3"><?php echo esc_html($data['title'.$i]) ?: '' ?></div></div>
+          <?php endif; ?>
           <p><?php echo urldecode($data['content'.$i]) ?: '' ?></p>
         </div>
       </div>
