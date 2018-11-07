6a8aaeca8977e5c4a9d57d67471a733612d8e988
jdelasoie <julien.delasoie@epfl.ch>
2018-11-07 10:15:32 +0100

    Update shortcodes/epfl_google_forms/view.php
    Co-Authored-By: LuluTchab <l.chaboudez@gmail.com>

--- shortcodes/epfl_google_forms/view.php
+++ shortcodes/epfl_google_forms/view.php
@@ -4,7 +4,7 @@
   $height  = get_query_var('epfl_google_forms_height');
 ?>
 
-<div class="container">
+<div class="container my-3">
     <iframe src="<?php echo esc_url($src); ?>" width="<?php echo esc_attr($width); ?>" height="<?php echo esc_attr($height); ?>" frameborder="0" marginheight="0" marginwidth="0">
         <?php echo __("Loading...", "epfl");?>
     </iframe>
