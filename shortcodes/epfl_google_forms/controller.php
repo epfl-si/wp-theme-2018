2e2117f99052120f0058964e4e0eb0886ab87336
LuluTchab <l.chaboudez@gmail.com>
2018-11-07 10:13:10 +0100

    Move 'loading' parameter from shortcode to theme and use of internal translation file

--- shortcodes/epfl_google_forms/controller.php
+++ shortcodes/epfl_google_forms/controller.php
@@ -5,9 +5,9 @@
  * 4rth argument is number of arguments the function can accept
  **/
 
-add_action('epfl_google_forms_action', 'renderGoogleForms', 10, 4);
+add_action('epfl_google_forms_action', 'renderGoogleForms', 10, 3);
 
-function renderGoogleForms ( $src, $width, $height, $loading) {
+function renderGoogleForms ( $src, $width, $height) {
 
   if (is_admin()) {
 
@@ -20,7 +20,6 @@ function renderGoogleForms ( $src, $width, $height, $loading) {
     set_query_var('epfl_google_forms_src', $src);
     set_query_var('epfl_google_forms_width', $width);
     set_query_var('epfl_google_forms_height', $height);
-    set_query_var('epfl_google_forms_loading', $loading);
     get_template_part('shortcodes/epfl_google_forms/view');
   }
 }
