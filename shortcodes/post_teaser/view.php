604b6fa5dac81083fc95d1b9aec3dc7b8e466ee8
GregLeBarbar <gregory.charmier.epfl.ch>
2018-11-14 15:28:44 +0100

    fix post teaser

--- shortcodes/post_teaser/view.php
+++ shortcodes/post_teaser/view.php
@@ -9,7 +9,9 @@ if (isset($data['gray'])) $postCount = $postCount - 1;
   <div class="container-full my-3<?php echo $gray ? ' bg-gray-100' : '' ?>">
     <div class="container">
       <div class="card-deck<?php echo ($postCount < 3) ? ' card-deck-line' : '' ?>">
-        <?php foreach($data as $key => $post) : ?>
+        <?php foreach($data as $key => $post) :
+            if ($key == 'gray') continue;
+        ?>
         <?php  $post_url = get_permalink($post); ?>
         <a href="<?php echo $post_url ?>" class="card link-trapeze-horizontal">
           <?php
