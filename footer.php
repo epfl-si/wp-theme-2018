cda975dc71637866e7d3b429abfef6a3de725c5a
Julien Delasoie <julien.delasoie@epfl.ch>
2019-01-21 17:43:02 +0100

    Add a language helper

--- footer.php
+++ footer.php
@@ -14,16 +14,7 @@
 	<?php
 		// TODO: Generate footer based on future webservice?
 		# fetch language
-		$default_lang = 'en';
-		$allowed_langs = array('en', 'fr');
-		$language = $default_lang;
-		/* If Polylang installed */
-		if(function_exists('pll_current_language'))
-		{
-			$current_lang = pll_current_language('slug');
-			// Check if current lang is supported. If not, use default lang
-			$language = (in_array($current_lang, $allowed_langs)) ? $current_lang : $default_lang;
-		}
+		$language = get_current_language();
 
 		if ($language === 'fr') {
 			require_once(__DIR__.'/footer_fr.php');
--- header.php
+++ header.php
@@ -43,17 +43,7 @@ class EPFL_Theme2018_Root_Menu_Walker extends Walker_Nav_Menu {
 ?>
 <?php
 	// TODO: Generate footer based on future webservice?
-	# fetch language
-	$default_lang = 'en';
-	$allowed_langs = array('en', 'fr');
-	$language = $default_lang;
-	/* If Polylang installed */
-	if(function_exists('pll_current_language'))
-	{
-		$current_lang = pll_current_language('slug');
-		// Check if current lang is supported. If not, use default lang
-		$language = (in_array($current_lang, $allowed_langs)) ? $current_lang : $default_lang;
-	}
+	$language = get_current_language();
 
 	if ($language === 'fr') {
 		$celebration_url = 'https://www.epfl.ch/campus/events/fr/celebration/';
