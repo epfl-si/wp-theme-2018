92a86229ab3a764aebb5774af9a647d92ee63948
Julien Delasoie <julien.delasoie@epfl.ch>
2019-03-11 14:07:14 +0100

    Remove turning 50e logo

--- header.php
+++ header.php
@@ -110,14 +110,9 @@ class EPFL_Theme2018_Root_Menu_Walker extends Walker_Nav_Menu {
 
 	<header role="banner" class="header">
 
-  	<div class="site-branding">
-  	    <a class="logo" href="<?php echo get_epfl_home_url(); ?>">
-  			<img src="<?php bloginfo('template_url'); ?>/assets/svg/epfl-logo.svg" alt="Logo EPFL, École polytechnique fédérale de Lausanne" class="img-fluid">
-  		</a>
-  		<a class="logo-50" href="<?php echo $celebration_url; ?>">
-    		<img src="<?php bloginfo('template_url'); ?>/theme/img/epfl-logo-50-150x111.gif" alt="Logo EPFL 50e anniversaire" width="101" height="91">
-  		</a>
-  	</div>
+	<a class="logo" href="<?php echo get_epfl_home_url(); ?>">
+		<img src="<?php bloginfo('template_url'); ?>/assets/svg/epfl-logo.svg" alt="Logo EPFL, École polytechnique fédérale de Lausanne" class="img-fluid">
+	</a>
 
 	<?php
 		global $EPFL_MENU_LOCATION;
