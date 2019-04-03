51b5d53e131fb31ad683bf5f9091b63e79d7560b
Aline <aline.keller@gmail.com>
2019-04-03 14:08:25 +0200

    Add 'wp-theme-light' child theme

---
+++ wp-theme-light/footer.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package epfl
+ */
+?>
+<div class="bg-gray-100 pt-5 mt-5">
+	<div class="container">
+	<?php
+		// TODO: Generate footer based on future webservice?
+		# fetch language
+		$language = get_current_language();
+
+		if ($language === 'fr') {
+			require_once(__DIR__.'/footer_fr.php');
+		} else {
+			require_once(__DIR__.'/footer_en.php');
+		}
+	?>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
+</div><!-- #page -->
+</body>
+</html>
---
+++ wp-theme-light/footer_fr.php
@@ -0,0 +1,53 @@
+<footer class="footer-light" role="contentinfo">
+  <div class="row">
+    <div class="col-6 mx-auto mx-md-0 mb-4 col-md-3 col-lg-2">
+      <a href="#">
+        <img src="<?php bloginfo('template_url'); ?>/assets/svg/epfl-logo.svg" alt="Logo EPFL, École polytechnique fédérale de Lausanne" class="img-fluid">
+      </a>
+    </div>
+    <div class="col-md-9 col-lg-10 mb-4">
+      <div class="ml-md-2 ml-lg-5">
+        <ul class="list-inline list-unstyled">
+          <li class="list-inline-item">Contact</li>
+          <li class="list-inline-item text-muted pl-3"><small>EPFL  CH-1015 Lausanne</small></li>
+          <li class="list-inline-item text-muted pl-3"><small>+41 21 693 11 11</small></li>
+        </ul>
+        <p class="footer-light-socials">
+                  <small>Suivez l'EPFL sur les réseaux sociaux</small>
+                          <span>
+            <a href="https://www.facebook.com/epflcampus" class="social-icon social-icon-facebook social-icon-negative">
+  <svg class="icon" aria-hidden="true"><use xlink:href="#icon-facebook"></use></svg>
+  <span class="sr-only">Follow us on Facebook.</span>
+</a>
+<a href="https://twitter.com/epfl" class="social-icon social-icon-twitter social-icon-negative">
+  <svg class="icon" aria-hidden="true"><use xlink:href="#icon-twitter"></use></svg>
+  <span class="sr-only">Follow us on Twitter.</span>
+</a>
+<a href="http://instagram.com/epflcampus" class="social-icon social-icon-instagram social-icon-negative">
+  <svg class="icon" aria-hidden="true"><use xlink:href="#icon-instagram"></use></svg>
+  <span class="sr-only">Follow us on Instagram.</span>
+</a>
+<a href="https://www.youtube.com/user/epflnews" class="social-icon social-icon-youtube social-icon-negative">
+  <svg class="icon" aria-hidden="true"><use xlink:href="#icon-youtube"></use></svg>
+  <span class="sr-only">Follow us on Youtube.</span>
+</a>
+<a href="https://www.linkedin.com/school/epfl/" class="social-icon social-icon-linkedin social-icon-negative">
+  <svg class="icon" aria-hidden="true"><use xlink:href="#icon-linkedin"></use></svg>
+  <span class="sr-only">Follow us on LinkedIn.</span>
+</a>
+          </span>
+        </p>
+        
+<div class="footer-legal">
+  <div class="footer-legal-links">
+    <a href="https://epfl-idevelop.github.io/elements/#/doc/accessibility.html">Accessibilité</a>
+    <a href="https://www.epfl.ch/about/overview/fr/reglements-et-directives/">Mentions légales</a>
+  </div>
+  <div>
+    <p>&copy; 2018 EPFL, tous droits réservés</p>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+</footer>
