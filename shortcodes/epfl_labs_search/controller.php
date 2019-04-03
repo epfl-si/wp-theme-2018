973449fa2c97029ca9c9b6694c8a5499248d20a5
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-03 16:03:13 +0200

    Send search text to the epfl plugin

--- shortcodes/epfl_labs_search/controller.php
+++ shortcodes/epfl_labs_search/controller.php
@@ -7,14 +7,29 @@
 
 add_action('epfl_labs_search_action', 'renderLabsSearch', 10, 0);
 
-wp_enqueue_script('jquery');
+function add_jquery() {
+  wp_enqueue_script('jquery');
+}
+add_action( 'wp_enqueue_scripts', 'add_jquery' );
 
 add_action('wp_ajax_labs_search_form', 'labs_search_form_submitted');
 add_action('wp_ajax_nopriv_labs_search_form', 'labs_search_form_submitted');
 
 function labs_search_form_submitted() {
   check_ajax_referer( 'epfl_labs_search' );
-  wp_send_json_success($_GET['labs_search_text']);
+  $search_text = $_GET['labs_search_text'];
+
+  if (empty($search_text)) {
+    wp_send_json_success('');
+  }
+
+  if (has_action("epfl_labs_search_action_callback"))
+  {
+    $sites = do_action('epfl_labs_search_action_callback', $search_text);
+    wp_send_json_success($sites);
+  } else {
+    wp_send_json_error('The callback is missing for the search action');
+  }
 }
 
 function renderLabsSearch() {
