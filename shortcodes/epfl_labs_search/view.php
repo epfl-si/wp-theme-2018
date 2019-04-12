f09842be26caf16049c1690d5a2aa2d45f670b75
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-12 10:18:06 +0200

    gs

--- shortcodes/epfl_labs_search/view.php
+++ shortcodes/epfl_labs_search/view.php
@@ -1,6 +1,8 @@
 <?php
     $sites = get_query_var('epfl_labs-sites');
     $predefined_tags = get_query_var('epfl_labs-predefined_tags');
+
+    $current_language = get_current_language();
 ?>
 
 <div class="container my-3">
@@ -32,14 +34,18 @@
     </div>
     <?php if (!(empty($sites))): ?>
         <?php foreach($sites as $site): ?>
-        <div class="row d-sm-flex pt-2 pb-2 border-bottom ">
-            <div class="col-2"><b><?php echo $site->title; ?></b></div>
-            <div class="col-5"><?php echo $site->tagline; ?></div>
-            <div class="col-3"><a href="<?php echo $site->url; ?>"><?php echo $site->url; ?></a></div>
+        <div class="row d-sm-flex pt-1 pb-1 border-bottom align-items-center">
+            <div class="col-2"><?php echo esc_html($site->title); ?></div>
+            <div class="col-5"><?php echo esc_html($site->tagline); ?></div>
+            <div class="col-3"><a href="<?php echo esc_html($site->url); ?>"><?php echo esc_html($site->url); ?></a></div>
             <?php if (!(empty($site->tags))): ?>
             <div class="col-2 pt-1">
                 <?php foreach($site->tags as $tag): ?>
-                <a href="<?php echo $tag->url_fr; ?>" class="tag tag-primary"><?php echo $tag->name_fr; ?></a>
+                    <?php if ($current_language === 'fr'): ?>
+                <a href="<?php echo esc_html($tag->url_fr); ?>" class="tag tag-primary"><?php echo esc_html($tag->name_fr); ?></a>
+                    <?php else: ?>
+                <a href="<?php echo esc_html($tag->url_en); ?>" class="tag tag-primary"><?php echo esc_html($tag->name_en); ?></a>
+                    <?php endif; ?>
                 <?php endforeach; ?>
             </div>
             <?php endif; ?>
