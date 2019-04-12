6caee5a346a3414f419538e5ca3f9bbc9cd83a40
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-12 14:27:56 +0200

    Fix path for js and css

--- shortcodes/epfl_labs_search/controller.php
+++ shortcodes/epfl_labs_search/controller.php
@@ -7,8 +7,8 @@
 
 add_action('epfl_labs_search_action', 'renderLabsSearch', 10, 2);
 
-wp_enqueue_script( 'epfl-labs-search-listjs', __DIR__. '/lib/list.js', ['jquery'], 1.5, true);
-wp_enqueue_style( 'epfl-labs-search-css',  __DIR__. '/epfl-labs-search.css',false,'1.0','all');
+wp_enqueue_script( 'epfl-labs-search-listjs', get_template_directory_uri() . '/shortcodes/epfl_labs_search/lib/list.js', ['jquery'], 1.5, true);
+wp_enqueue_style( 'epfl-labs-search-css', get_template_directory_uri() . '/shortcodes/epfl_labs_search/epfl-labs-search.css',false,'1.0','all');
 
 /**
  * render the shortcode, mainly a form and his table
