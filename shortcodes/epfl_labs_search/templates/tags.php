9093677e24dbf1ebe31b960cf9a9e71a635f3fef
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-15 13:57:38 +0200

    Remove unused language translation

--- shortcodes/epfl_labs_search/templates/tags.php
+++ shortcodes/epfl_labs_search/templates/tags.php
@@ -5,18 +5,7 @@
  */
 ?>
 
-<?php
-    $current_language = get_current_language();
-    if ($current_language === 'fr') {
-        echo "let name_field = 'name_fr';\n";
-        echo "let url_field = 'url_fr';\n";
-    } else {
-        echo "let name_field = 'name_en';\n";
-        echo "let url_field = 'url_en';\n";
-    }
-?>
-    
 for (j = 0; j < site['tags'].length; j++) {
     let tag = site['tags'][j];
-    rendered += `<a href="${tag[url_field]}" class="labs-search-site-tag-link">${tag[name_field]}</a>&nbsp;`;
+    rendered += `<a href="${tag[url}" class="labs-search-site-tag-link">${tag[name]}</a>&nbsp;`;
 }
