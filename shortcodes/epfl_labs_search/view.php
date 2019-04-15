b1bb4849fb19e5023d581e34cc2bff5632d58cb2
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-15 12:39:52 +0200

    Add translations; simplify texts

--- shortcodes/epfl_labs_search/view.php
+++ shortcodes/epfl_labs_search/view.php
@@ -7,12 +7,11 @@
 <div class="container my-3">
     <div id="sites-list" class="d-flex flex-column">
         <div class="form-group">
-            <div class="">
-                <label><?php _e('Search on', 'epfl'); ?></label>
+            <div>
                 <?php foreach($combo_list_content as $type => $names): ?>
                     <select
                         id="select-<?php echo esc_html($type); ?>"
-                        class="epfl-labs-select custom-select"
+                        class="epfl-labs-select custom-select mb-2"
                     >
                         <option <?php echo (empty($predefined_faculty))?"selected":"";?> value="all">
                         <?php
@@ -40,7 +39,6 @@
                     </select>
                 <?php endforeach; ?>
 
-                <label for="labs-search-input"><?php _e('Search for', 'epfl'); ?></label>
                 <input
                     type="text"
                     id="labs-search-input"
@@ -48,15 +46,11 @@
                     placeholder="<?php _e('Type here a name, an url, a keyword, ...', 'epfl') ?>"
                     aria-describedby="labs-search-input-help"
                 >
-                <small id="labs-search-input-help" class="form-text text-muted">
-                    <?php _e('Laboratories are part of faculties and institutes, and have search domains.', 'epfl') ?>
-                </small>
             </div>
-
         </div>
         <div id="sorting-header" class="flex-row d-md-flex pt-1 pb-1 border-bottom align-items-center mb-2">
-                <div class="sort col-2" data-sort="site-title"><a href="#">Acronym</a></div>
-                <div class="sort col-7" data-sort="site-tagline"><a href="#">Title</a></div>
+                <div class="sort col-2" data-sort="site-title"><a href="#"><?php _e('Acronym') ?></a></div>
+                <div class="sort col-7" data-sort="site-tagline"><a href="#"><?php _e('Title') ?></a></div>
         </div>
         <div class="list">
 
