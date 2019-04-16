64660ab1a14052a9cfe43033a27b903ec7d222d1
Julien Delasoie <julien.delasoie@epfl.ch>
2019-04-16 11:13:35 +0200

    Update css version

--- shortcodes/epfl_labs_search/controller.php
+++ shortcodes/epfl_labs_search/controller.php
@@ -8,7 +8,7 @@
 add_action('epfl_labs_search_action', 'renderLabsSearch', 10, 3);
 
 wp_enqueue_script( 'epfl-labs-search-listjs', get_template_directory_uri() . '/shortcodes/epfl_labs_search/lib/list.min.js', ['jquery'], 1.5, true);
-wp_enqueue_style( 'epfl-labs-search-css', get_template_directory_uri() . '/shortcodes/epfl_labs_search/epfl-labs-search.css',false,'1.0','all');
+wp_enqueue_style( 'epfl-labs-search-css', get_template_directory_uri() . '/shortcodes/epfl_labs_search/epfl-labs-search.css',false,'1.1','all');
 
 /**
  * render the shortcode, mainly a form and his table
