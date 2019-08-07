3a7c4a296c31a3ec5bca878b500190cb4510da80
Julien Delasoie <julien.delasoie@epfl.ch>
2019-08-07 16:54:18 +0200

    Improve presentation

--- wp-theme-2018/shortcodes/epfl_polylex_search/view.php
+++ wp-theme-2018/shortcodes/epfl_polylex_search/view.php
@@ -12,7 +12,7 @@
                 type="text"
                 id="lexes-search-input"
                 class="form-control search mb-2"
-                placeholder="<?php _e('Type here a name, a number, a keyword, ...', 'epfl') ?>"
+                placeholder="<?php _e('Type here a a number or a keyword', 'epfl') ?>"
                 aria-describedby="lexes-search-input-help"
             >
             <div id="selects-filter" class="d-flex flex-wrap flex-column flex-md-row">
@@ -49,27 +49,31 @@
             </div>
         </div>
         <div id="sorting-header" class="flex-row d-md-flex pt-1 pb-1 border-bottom align-items-center mb-2">
-                <div class="sort col-1" data-sort="lex-number"><a href="#" onclick="return false;"><?php _e('Number', 'epfl') ?></a></div>
-                <div class="sort col-7" data-sort="lex-title"><a href="#" onclick="return false;"><?php _e('Title', 'epfl') ?></a></div>
-                <div class="sort col-4" data-sort="lex-category-subcategory"><a href="#" onclick="return false;"><?php _e('Category', 'epfl') ?></a></div>
+                <div class="sort col-1" data-sort="lex-number"><a href="#" onclick="return false;"><strong><?php _e('Number', 'epfl') ?></strong></a></div>
+                <div class="sort col-7" data-sort="lex-title"><a href="#" onclick="return false;"><strong><?php _e('Title', 'epfl') ?></strong></a></div>
+                <div class="sort col-4" data-sort="lex-category-subcategory"><a href="#" onclick="return false;"><strong><?php _e('Category', 'epfl') ?></strong></a></div>
         </div>
 
         <div class="lex-list">
         <?php if (!(empty($lexes))): ?>
             <?php foreach($lexes as $key => $lex): ?>
-            <div class="lex-row mb-0 mt-0 pb-2 pt-2 border-bottom border-top align-items-center">
-                <div class="lex-row-1 flex-row d-md-flex">
+            <div class="lex-row mb-0 mt-0 pb-3 pt-3 border-bottom border-top align-items-center">
+                <div class="lex-row-1 flex-row d-md-flex pt-1 pb-1">
                     <div class="lex-number col-1"><?php echo esc_html($lex->lex); ?></div>
-                    <div class="col-7"><a class="lex-url" href="<?php echo esc_html($lex->url); ?>"><span class="lex-title"><?php echo esc_html($lex->title); ?></span></a></div>
+                    <div class="col-7"><a class="lex-url" href="<?php echo esc_html($lex->url); ?>"><span class="lex-title"><strong><?php echo esc_html($lex->title); ?></strong></span></a></div>
                     <?php if (!(empty($lex->category))): ?>
-                    <div class="lex-category-subcategory col-4 pt-1 d-flex flex-row" data-category-subcategory="<?php echo esc_html($lex->category) . esc_html($lex->subcategory); ?>"><?php echo esc_html($lex->category); ?><?php if (!(empty($lex->subcategory))): ?><?php echo ', ' . esc_html($lex->subcategory); ?><?php endif; ?></div>
+                    <div class="lex-category-subcategory col-4 " data-category-subcategory="<?php echo esc_html($lex->category) . esc_html($lex->subcategory); ?>"><?php echo esc_html($lex->category); ?><?php if (!(empty($lex->subcategory))): ?><?php echo ', ' . esc_html($lex->subcategory); ?><?php endif; ?></div>
                     <?php endif; ?>
                 </div>
-                <div class="lex-row-2 flex-row d-md-flex pt-1 pb-1">
-                    <div class="col-1"></div>
-                    <div class="lex-publicationDate col-3"><?php echo esc_html($lex->publicationDate); ?></div>
+                <div class="lex-row-2 flex-row d-md-flex pt-2 pb-2">
+                <div class="col-1"></div>
+                    <div class="col"><em><?php echo esc_html($lex->description); ?></em></div>
+                </div>
+                <div class="lex-row-3 flex-row d-md-flex pt-1 pb-1">
+                <div class="col-1"></div>
+                    <div class="lex-publicationDate col-4"><?php echo esc_html($lex->publicationDate); ?></div>
                     <?php if (!(empty($lex->authors))): ?>
-                    <div class="lex-authors col-4 d-md-flex flex-row">
+                    <div class="lex-authors col d-md-flex flex-row">
                         <?php foreach($lex->authors as $index => $author): ?>
                         <div class="mr-2">
                         <a href="<?php echo esc_html($author->url); ?>" class="author lex-author"><?php echo esc_html($author->firstName) .'&nbsp;'. esc_html($author->lastName) ; ?></a>
@@ -77,10 +81,7 @@
                         <?php endforeach; ?>
                     </div>
                     <?php endif; ?>
-                </div>
-                <div class="lex-row-3 flex-row d-md-flex pt-1 pb-1">
-                    <div class="col-1"></div>
-                    <div class="col"><?php echo esc_html($lex->description); ?></div>
+
                 </div>
             </div>
             <?php endforeach; ?>
