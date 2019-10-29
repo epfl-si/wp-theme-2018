39b828241edefad39edd0985ca9399c5bfaf2e4c
Julien Delasoie <julien.delasoie@epfl.ch>
2019-10-29 20:58:52 +0100

    First commit to remove some unused shortcode/functions

--- wp-theme-2018/shortcodes/epfl_video/view.php
+++
@@ -1,9 +0,0 @@
-<?php
-  $url = get_query_var('epfl_video_url');
-?>
-
-<div class="container my-3">
-  <div class="embed-responsive embed-responsive-16by9">
-    <iframe src="<?php echo esc_url($url) ?>" webkitallowfullscreen mozallowfullscreen allowfullscreen allow="autoplay; encrypted-media" frameborder="0" class="embed-responsive-item"></iframe>
-  </div>
-</div>
