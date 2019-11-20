5eccdbd96be4578180beb86d15ddd79824fb4a19
Julien Delasoie <julien.delasoie@epfl.ch>
2019-11-20 17:02:29 +0100

    Move epfl_except

--- wp-theme-2018/functions.php
+++ wp-theme-2018/functions.php
@@ -273,26 +273,6 @@ function excerpt_more( $more = '') {
 add_filter( 'excerpt_more', 'excerpt_more' );
 
 /**
- * custom function for epfl excerpts. With a given post, returns a formatted excerpt.
- * it will have the same behaviour whether it takes a user-defined excerpt or generates one from the content
- * @arg $post a WP_Post object
- */
-function epfl_excerpt($post = null) {
-	if(!$post) return '';
-	$excerpt = $post->post_excerpt;
-	if (strlen($excerpt) == 0) {
-		// custom excerpt is empty, let's generate one
-		$excerpt = strip_shortcodes($post->post_content);
-		$excerpt = str_replace(array("\r\n", "\r", "\n", "&nbsp;"), "", $excerpt);
-		$excerpt = wp_trim_words($excerpt, custom_excerpt_length(), excerpt_more());
-	} else {
-		// custom excerpt is set, let's trim it
-		$excerpt = wp_trim_words($excerpt, custom_excerpt_length(), excerpt_more());
-	}
-	return $excerpt;
-}
-
-/**
  * Share icons directory with templates
  */
 global $iconDirectory;
