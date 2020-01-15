2a2c1723074acd7edb9ce3e709f83831332e896f
Julien Delasoie <julien.delasoie@epfl.ch>
2020-01-15 14:31:45 +0100

    Add JS to comboxboxes

--- wp-theme-2018/template-parts/blog-filters.php
+++ wp-theme-2018/template-parts/blog-filters.php
@@ -17,7 +17,7 @@
   </div>
   <div class="collapse collapse-lg-show collapse-drop" id="form-filters">
     <form class="p-3 p-lg-0 mt-md-3">
-      
+
       <div class="form-group">
         <div class="form-group">
           <label>Catégories</label>
@@ -30,15 +30,25 @@
             );
             wp_dropdown_categories( $args );
           ?>
+          <script type="text/javascript">
+              var dropdownCat = document.getElementById("cat");
+
+              function onCatChange() {
+                  if ( dropdownCat.options[dropdownCat.selectedIndex].value > 0 ) {
+                      location.href = "<?php echo esc_url( home_url( '/' ) ); ?>?cat="+dropdownCat.options[dropdownCat.selectedIndex].value;
+                  }
+              }
+              dropdownCat.onchange = onCatChange;
+          </script>
         </div>
       </div>
-      
+
       <div class="form-group">
         <div class="form-group">
           <label>Archives</label>
-          <select class="custom-select">
+          <select class="custom-select" id="year">
             <option selected><?php _e( 'Select a year', 'epfl' ); ?></option>
-            <?php 
+            <?php
               $args = array(
                   'type' => 'yearly',
                   'format' => 'option',
@@ -48,9 +58,18 @@
               wp_get_archives( $args );
             ?>
           </select>
+          <script type="text/javascript">
+              var dropdownYear = document.getElementById("year");
+              function onYearChange() {
+                  if ( dropdownYear.options[dropdownYear.selectedIndex].value ) {
+                      location.href = dropdownYear.options[dropdownYear.selectedIndex].value;
+                  }
+              }
+              dropdownYear.onchange = onYearChange;
+          </script>
         </div>
       </div>
-      
+
     </form>
   </div>
 </aside>
