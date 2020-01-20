b918680f732c2acf52a3c57e6f27c7f7e09a3882
Julien Delasoie <julien.delasoie@epfl.ch>
2020-01-20 12:40:33 +0100

    Remove forgotten var_dump

--- wp-theme-2018/menus/blog-posts.php
+++ wp-theme-2018/menus/blog-posts.php
@@ -48,7 +48,6 @@ function build_a_root_posts_menu_item_from_scratch($items) {
     if (function_exists('pll_default_language')) {
         // check if we need the particule or the default page
         if ($language != pll_default_language()) {
-            var_dump(pll_default_language());
             $particule = '/' . $language;
         }
     }
