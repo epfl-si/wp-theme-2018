86a4a56107c2250eadaba911226e9b6ccafd71e4
Julien Delasoie <julien.delasoie@epfl.ch>
2020-01-22 10:44:46 +0100

    Fix the last card in blog list being too big if alone

--- wp-theme-2018/template-parts/blog-list.php
+++ wp-theme-2018/template-parts/blog-list.php
@@ -1,8 +1,24 @@
 <?php if (have_posts()) : ?>
   <div class="card-deck">
-  <?php while ( have_posts() ) : the_post();
+  <?php
+  // assert we always finish in a full row
+  $post_idx = 0;
+
+  while ( have_posts() ) :
+    the_post();
     get_template_part( 'template-parts/content', 'post-card' );
-  endwhile; // End of the loop. ?>
+    $post_idx += 1;
+  endwhile;
+
+  // fullfil the row if we have at least one full
+  if ( $post_idx > 3 ) {
+    while ( $post_idx % 3 != 0 ) {
+      echo "<div></div>";
+      $post_idx += 1;
+    }
+  }
+
+  ?>
   </div>
 <?php else : ?>
   <h2>
