14efb7eb293794b0702e563943afd6dc77fbcdc9
Rosa Maggi <rosa.maggi@epfl.ch>
2023-11-10 16:01:16 +0100

    [Feature] Add new style and insert cache for api calls
    Two transient have been created: a short one of 1 hour and a long one
    1 week.
    The key of the transient is given by:
    - current url
    - current language
    - the type of the call [breadcrumbs, siblings]
    - the interval [short, long]

--- wp-theme-breadcrumb/template-parts/breadcrumb.php
+++ wp-theme-breadcrumb/template-parts/breadcrumb.php
@@ -123,14 +123,16 @@ function get_current_item($items) {
     return $item;
 }
 
-function get_rendered_crumb_item($crumb_item, $is_current_item=False) {
+function get_rendered_crumb_item($crumb_item, $is_current_item=False, $siblings_items) {
     $title = $crumb_item['title'] ?? '';
     $url = $crumb_item['url'] ?? '';
 
+    $siblings = render_siblings($siblings_items);
     if ( $is_current_item ) {
         return "
         <li class=\"breadcrumb-item active\" aria-current=\"page\">
             {$title}
+            {$siblings}
         </li>";
     } else {
         return "
@@ -138,49 +140,120 @@ function get_rendered_crumb_item($crumb_item, $is_current_item=False) {
             <a class=\"bread-link bread-home\" href=\"{$url}\" title=\"{$title}\">
                 {$title}
             </a>
+            {$siblings}     
         </li>";
     }
 }
 
-function call_service($url): array
+function render_siblings($siblings_items) {
+    $siblings = '';
+    foreach ($siblings_items as $sibling) {
+        $siblings = $siblings. "<li class=\"dropdown-item\"><a href=\"{$sibling['url']}\">{$sibling['title']}</a></li>";
+    }
+    if ($siblings !== '') {
+        return "
+            <div class=\"dropdown\">
+                <button class=\"btn btn-secondary dropdown-toggle\" type=\"button\" id=\"dropdownMenuButton\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">
+                    <span class=\"sr-only\">Affiche les pages de même niveau</span>
+                </button>
+                <ul class=\"dropdown-menu\" aria-labelledby=\"dropdownMenuButton\">
+                    {$siblings}
+                </ul>
+            </div>";
+    }
+    return $siblings;
+}
+
+function call_service($urlSite, $lang,$callType): array
 {
-    //print ($url);
-    $curl = curl_init($url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    $urlApi = 'http://menu-api:3000/menus/'.$callType.'/?lang=' . $lang . '&url=' . trailingslashit( $urlSite );
+    $shortCacheRefreshInterval = 7 * DAY_IN_SECONDS;
+    $longCacheRefreshInterval = 60 * MINUTE_IN_SECONDS;
+    $shortCacheParameters = [
+        "siteUrl" => $urlSite,
+        "lang" => $lang,
+        "callType" => $callType,
+        "cacheInterval" => 'short'
+    ];
+    $longCacheParameters = [
+        "siteUrl" => $urlSite,
+        "lang" => $lang,
+        "callType" => $callType,
+        "cacheInterval" => 'long'
+    ];
 
-    $response = curl_exec($curl);
-    if (curl_errno($curl)) {
-        echo 'cURL error: ' . curl_error($curl). ': '. $url;
-    }
+    $shortSerializedParameters = serialize($shortCacheParameters);
+    $longSerializedParameters = serialize($longCacheParameters);
+    $shortTransientName = "menu-api-{$shortSerializedParameters}";
+    $longTransientName = "menu-api-{$longSerializedParameters}";
 
-    curl_close($curl);
-    if ($response === false) {
-        echo 'Failed to retrieve data from the API.';
-    } else {
-        $data = json_decode($response, true)['result'];
-        if ($data !== null) {
-            return $data;
-        } else {
-            echo 'Failed to parse JSON response.';
+    $res = get_transient($shortTransientName);
+
+    if (false === $res) {  //if the given short transient is empty we call the API
+        //print ($url);
+        $curl = curl_init($urlApi);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($curl);
+        if (curl_errno($curl)) {
+            //TODO write logs somewhere ---- echo 'cURL error: ' . curl_error($curl). ': '. $urlApi;
         }
+        curl_close($curl);
+
+        if ($response === false) {
+            //TODO write logs somewhere ---- echo 'Failed to retrieve data from the API.';
+        } else {
+            $data = json_decode($response, true)['result'];
+            if ($data !== null) {  //With the API response we set both transients
+                set_transient($shortTransientName, $data, $shortCacheRefreshInterval);
+                set_transient($longTransientName, $data, $longCacheRefreshInterval);
+                return $data;
+            } else {
+                //TODO write logs somewhere ---- echo 'Failed to parse JSON response.';
+            }
+        }
+        //if we have an API error, we get the long transient response, if we have one
+        $res = get_transient($longTransientName);
+        if (false !== $res) {
+            return $res;
+        }
+        return [];
+    } else {
+        return $res;
     }
-    return [];
 }
 ?>
+<style>
+    .breadcrumb-wrapper {
+        overflow: revert;
+    }
+    .breadcrumb .dropdown {
+        display: inline;
+        position: static;
+    }
+    .breadcrumb .dropdown-toggle {
+        border: 0;
+        padding: 0;
+        height: 1.5rem;
+        margin-left: 0.25rem;
+        width: 1rem;
+    }
+    .breadcrumb .dropdown-toggle::after {
+        margin: 0;
+    }
+    .breadcrumb .dropdown-menu.show {
+        left: -10px !important;
+        top: 20px !important;
+        transform: none !important;
+        min-width: 100%;
+    }
+    .breadcrumb .dropdown-item {
+        font-size: 0.83rem;
+        padding: 0.125em 0.625rem;
+    }
+</style>
+
 <div class="breadcrumb-container">
-    <!-- Browse  -->
-    <div>
-        <button id="nav-toggle" class="nav-toggle btn btn-secondary">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-browse"></use>
-            </svg>
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="#icon-close"></use>
-            </svg>
-            <?php echo __("Browse", 'epfl') ?>
-        </button>
-    </div>
-    <!-- end Browse -->
 
     <!-- Breadcrumb -->
     <nav aria-label="breadcrumb" class="breadcrumb-wrapper" id="breadcrumb-wrapper">
@@ -220,16 +293,16 @@ function call_service($url): array
             //get_site_url() return the site url and not the current page url
             $protocol = is_ssl() ? 'https://' : 'http://';
             $url = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            $parent_items = call_service('http://menu-api:3000/menus/breadcrumb/?lang=' . $current_lang . '&url=' . trailingslashit( $url ));
-            $crumb_items = [...$parent_items]; # ...$crumb_items
+            $parent_items = call_service($url, $current_lang, 'breadcrumb');
 
-            foreach($crumb_items as $crumb_item) {
+            foreach($parent_items as $crumb_item) {
+                $siblings_items = call_service($crumb_item['url'], $current_lang, 'siblings');
                 $current_item_db_id = $current_item->db_id ?? null;
                 $crumb_item_db_id = $crumb_item['db_id'] ?? null;
                 if ($current_item_db_id && $crumb_item_db_id && (int) $current_item_db_id === (int) $crumb_item_db_id) { // current item ?
-                    $crumbs[] = get_rendered_crumb_item($crumb_item, True);
+                    $crumbs[] = get_rendered_crumb_item($crumb_item, True, $siblings_items);
                 } else {
-                    $crumbs[] = get_rendered_crumb_item($crumb_item, False);
+                    $crumbs[] = get_rendered_crumb_item($crumb_item, False, $siblings_items);
                 }
             }
 
