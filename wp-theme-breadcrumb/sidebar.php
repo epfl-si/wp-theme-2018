a99e3aec3b636716bb7c6a678d92f9a76515284c
Rosa Maggi <rosa.maggi@epfl.ch>
2024-05-29 15:53:18 +0200

    [Fix] Correct sidebar

--- wp-theme-breadcrumb/sidebar.php
+++ wp-theme-breadcrumb/sidebar.php
@@ -20,53 +20,52 @@ $items = wp_get_nav_menu_items(get_current_menu_slug());
 $current_menu_entry = get_menu_entry_from_element_id($items, get_currently_viewed_element_id());
 
 if (get_queried_object() && !is_archive()) {  // only when we are looking at an object or in the category/tag view
-	if ($current_menu_entry === false || $current_menu_entry->menu_item_parent == 0 || is_home()) {
-		$classes = 'current-menu-parent';
-	}
+    if ($current_menu_entry === false || $current_menu_entry->menu_item_parent == 0 || is_home()) {
+        $classes = 'current-menu-parent';
+    }
 }
 
 ?>
-<div class="nav-container">
-    <nav class="nav-main">
-        <div class="nav-wrapper">
-            <div class="nav-container <?php echo $classes ?>">
-                <ul id="menu-main" class="nav-menu">
-                    <li class=""><a href="#">À propos</a></li>
-                    <li><a href="#">Éducation</a></li>
-                    <li><a href="#">Recherche</a></li>
-                    <li><a href="#">Innovation</a></li>
-                    <li class="current-menu-item"><a href="#">Facultés</a></li>
-                    <li><a href="#">Campus</a></li>
-                </ul>
-            </div>
+<div class="overlay"></div>
+<nav class="nav-main">
+    <div class="nav-wrapper">
+        <div class="nav-container <?php echo $classes ?>">
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => $EPFL_MENU_LOCATION,
+                'menu_class'=> 'nav-menu',
+                'container' => 'ul',
+                'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+                'walker' => new Custom_Nav_Walker()
+            ) );
+            ?>
         </div>
-    </nav>
-
+    </div>
+</nav>
 
 <?php
-	$aside = true;
-	$asideContent = 'all';
-	$currentTemplate = get_page_template_slug();
-	if ($currentTemplate == 'page-aside-none.php' || $currentTemplate == 'page-homepage.php') $aside = false;
-	if ($currentTemplate == 'page-aside-siblings-only.php') $asideContent = 'siblings';
-	if ($currentTemplate == 'page-aside-children-only.php') $asideContent = 'children';
-	if ($aside) :
-?>
+$aside = true;
+$asideContent = 'all';
+$currentTemplate = get_page_template_slug();
+if ($currentTemplate == 'page-aside-none.php' || $currentTemplate == 'page-homepage.php') $aside = false;
+if ($currentTemplate == 'page-aside-siblings-only.php') $asideContent = 'siblings';
+if ($currentTemplate == 'page-aside-children-only.php') $asideContent = 'children';
+if ($aside) :
+    ?>
 
     <aside class="nav-aside-wrapper">
-	<nav id="nav-aside" class="nav-aside" role="navigation" aria-describedby="nav-aside-title">
-  	<h2 class="h5 sr-only-xl"><?php esc_html_e("In the same section", 'epfl') ?></h2>
-				<?php
-				wp_nav_menu( array(
-				    'theme_location' => $EPFL_MENU_LOCATION,
-        		    'menu_class'=> 'nav-menu',
-					'container' => 'ul',
-					'submenu' => get_the_ID(),
-					'submenu_type' => $asideContent
-				) );
-			?>
-	</nav>
-</aside>
+        <nav id="nav-aside" class="nav-aside" role="navigation" aria-describedby="nav-aside-title">
+            <h2 class="h5 sr-only-xl"><?php esc_html_e("In the same section", 'epfl') ?></h2>
+            <?php
+            wp_nav_menu( array(
+                'theme_location' => "",
+                'menu_class'=> 'nav-menu',
+                'container' => 'ul',
+                'submenu' => get_the_ID(),
+                'submenu_type' => $asideContent
+            ) );
+            ?>
+        </nav>
+    </aside>
 
 <?php endif; ?>
-</div>
