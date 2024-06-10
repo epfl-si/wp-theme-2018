ad31c8bca1c2299d6b877b3dc39190cd5d8cdd41
Rosa Maggi <rosa.maggi@epfl.ch>
2024-06-10 09:40:15 +0200

    [Fix] elements version

--- wp-theme-2018/assets/js/elements.js
+++ wp-theme-2018/assets/js/elements.js
@@ -7859,8 +7859,24 @@ var es_string_iterator = __webpack_require__(78783);
 /* globals $ */
 /* harmony default export */ var breadcrumb = (function () {
   var expandBreadcrumb = $('.btn-expand-links');
-  expandBreadcrumb.click(function () {
-    $(".breadcrumb-wrapper .breadcrumb").addClass("has-expanded-links");
+  var breadcrumbDropToggle = $('.dropdown-toggle'); // add class 'has-expanded-links'
+
+  expandBreadcrumb.on('click', function () {
+    $('.breadcrumb-wrapper .breadcrumb').addClass('has-expanded-links');
+  }); // improve dropdown position
+
+  breadcrumbDropToggle.on('click', function (e) {
+    var btnPos = $(e.currentTarget).offset().left;
+    var documentWitdh = $(document).width();
+    var dropdown = $(e.currentTarget).siblings('.dropdown-menu');
+    var dropdownWidth = dropdown.width();
+    var btnOffset = documentWitdh - btnPos; // remove class 'open-left' from all .dropdown-menu elements
+
+    $('.dropdown-menu').removeClass('open-left'); // add the class back if the dropdown is too close to the right side of the window
+
+    if (dropdownWidth > btnOffset) {
+      dropdown.addClass('open-left');
+    }
   });
 });
 ;// CONCATENATED MODULE: ./assets/components/molecules/datepicker/datepicker.js
