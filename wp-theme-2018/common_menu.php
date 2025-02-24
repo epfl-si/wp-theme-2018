0844fd277fbf7f9966e90a0cd3aaa6b2c0ef39d4
Rosa Maggi <rosa.maggi@epfl.ch>
2025-02-24 15:41:32 +0100

    [fix] spaces

--- wp-theme-2018/common_menu.php
+++ wp-theme-2018/common_menu.php
@@ -11,15 +11,15 @@ function call_menu_api_microservice($home_page_url, $url_site, $lang, $call_type
     $main_post_page_name = urlencode(get_the_title($current_language_page_id));
     $main_post_page_url = get_permalink($current_language_page_id);
 
-    $urlApi = 'http://' . (getenv('MENU_API_HOST') ?? "menu-api") . ':3001/menus/'.$call_type.'/?lang=' . $lang . '&url=' . trailingslashit( $url_site ) .
-    '&pageType=' . get_post_type() .
+    $urlApi = 'http://' . (getenv('MENU_API_HOST') ?? "menu-api") . ':3001/menus/' . $call_type . '/?lang=' . $lang
+    . '&url=' . trailingslashit( $url_site ) . '&pageType=' . get_post_type() .
     ($main_post_page == 0 ? '' : ($main_post_page_name == '' ? '' : '&mainPostPageName=' . $main_post_page_name)) .
-    ($main_post_page == 0 ? '' : ($main_post_page_url == '' ? '' : '&mainPostPageUrl=' . $main_post_page_url)).
+    ($main_post_page == 0 ? '' : ($main_post_page_url == '' ? '' : '&mainPostPageUrl=' . $main_post_page_url)) .
     '&postName=' . urlencode(get_the_title()) .
     '&homePageUrl=' . $home_page_url;
+
     $curl = curl_init($urlApi);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
     $response = curl_exec($curl);
     if (curl_errno($curl)) {
         $error_text = curl_error($curl);
