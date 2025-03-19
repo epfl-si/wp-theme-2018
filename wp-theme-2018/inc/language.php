0c6366e5a5f1733d276e129b730989075b41c021
Rosa Maggi <rosa.maggi@epfl.ch>
2025-03-19 12:14:09 +0100

    [fix] get default langiage only if Polylang is not installed
    Do not filter on allowed_lang

--- wp-theme-2018/inc/language.php
+++ wp-theme-2018/inc/language.php
@@ -3,16 +3,9 @@
  * get_current_language
  */
 function get_current_language ($default_lang='en') {
-	# fetch language
-	$allowed_langs = array('en', 'fr', 'de');
-	$language = $default_lang;
-
-	/* If Polylang installed */
-	if(function_exists('pll_current_language'))
-	{
-		$current_lang = pll_current_language('slug');
-		// Check if current lang is supported. If not, use default lang
-		$language = (in_array($current_lang, $allowed_langs)) ? $current_lang : $default_lang;
+	if ( function_exists('pll_current_language') ) {
+		return pll_current_language();
+	} else {
+		return $default_lang;
 	}
-	return $language;
 }
